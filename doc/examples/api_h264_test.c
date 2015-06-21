df6fdd051f19cc34aa085404b5becc5deba7f049
Ludmila Glinskih <lglinskih@gmail.com>
2015-06-21 21:06:53 +0300

    correct output to stdout, no memory leaks.

--- doc/examples/api_h264_test.c
+++ doc/examples/api_h264_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001 Fabrice Bellard
+ * Copyright (c) 2015 Ludmila Glinskih
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,37 +26,28 @@
 
 #include <math.h>
 
-#include <libavutil/opt.h>
-#include <libavutil/adler32.h>
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/channel_layout.h>
-#include <libavutil/common.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/mathematics.h>
-#include <libavutil/samplefmt.h>
-
-
-/*
- * Video decoding example
- */
+#include "libavutil/opt.h"
+#include "libavutil/adler32.h"
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavutil/common.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/samplefmt.h"
+#include "libavformat/internal.h"
 
 static int video_decode_example(const char *input_filename)
 {
     AVCodec *codec = NULL;
     AVCodecContext *origin_ctx = NULL, *ctx= NULL;
-    FILE *f = NULL;
     AVFrame *fr = NULL;
-    uint8_t *buffer = NULL, *byte_buffer = NULL;
+    uint8_t *byte_buffer = NULL;
     AVPacket pkt;
     AVFormatContext *fmt_ctx = NULL;
     int number_of_written_bytes;
-    int i;
     int video_stream;
-    int num_bytes;
     int get_frame = 0;
     int byte_buffer_size;
-    int if_read_frame;
+    int i = 0;
 
 
     if (avformat_open_input(&fmt_ctx, input_filename, NULL, NULL) < 0) {
@@ -64,12 +55,11 @@ static int video_decode_example(const char *input_filename)
         exit(1);
     }
 
-    printf("file is okay\n");
     if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
         fprintf(stderr, "Could not find stream information\n");
         exit(1);
     }
-    printf("stream is opened\n");
+
     video_stream = -1;
     for (i = 0; i < fmt_ctx->nb_streams; i++) {
         if (fmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
@@ -84,7 +74,6 @@ static int video_decode_example(const char *input_filename)
     if (codec == NULL) {
         return -1;
     }
-    printf("Found decode\n");
     ctx = avcodec_alloc_context3(codec);
     if (ctx == NULL) {
         return -1;
@@ -97,68 +86,69 @@ static int video_decode_example(const char *input_filename)
     if (avcodec_open2(ctx, codec, NULL) < 0) {
         return -1;
     }
-    printf("alloc codec and context\n");
+
     fr = av_frame_alloc();
-    if (fr == NULL)
+    if (fr == NULL) {
         return -1;
+    }
 
-    num_bytes = avpicture_get_size(ctx->pix_fmt, ctx->width, ctx->height);
-    buffer = (uint8_t*)av_malloc(num_bytes * sizeof(uint8_t));
-
-    avpicture_fill((AVPicture*)fr, buffer, ctx->pix_fmt, ctx->width, ctx->height);
-
-    //some staff with picture
     byte_buffer_size = av_image_get_buffer_size(ctx->pix_fmt, ctx->width, ctx->height, 1);
-    printf("%i -- buffer size\n", byte_buffer_size);
-    byte_buffer = (uint8_t*)av_malloc(byte_buffer_size * sizeof(uint8_t) + FF_INPUT_BUFFER_PADDING_SIZE);
+    byte_buffer = (uint8_t*)av_malloc(byte_buffer_size * sizeof(uint8_t));
 
+    printf("#tb %d: %d/%d\n", video_stream, fmt_ctx->streams[video_stream]->time_base.num, fmt_ctx->streams[video_stream]->time_base.den);
+    i = 0;
     av_init_packet(&pkt);
-    printf("starting to decode frames\n");
     while (av_read_frame(fmt_ctx, &pkt) >= 0) {
         if (pkt.stream_index == video_stream) {
             get_frame = 0;
+            if (pkt.pts == AV_NOPTS_VALUE)
+                pkt.pts = pkt.dts = i;
             avcodec_decode_video2(ctx, fr, &get_frame, &pkt);
-            av_free_packet(&pkt);
-            av_init_packet(&pkt);
-            printf("YEAAAH WE DECODED IT\n");
             if (get_frame) {
-                printf("we finally get frame\n");
                 number_of_written_bytes = av_image_copy_to_buffer(byte_buffer, byte_buffer_size,
                                         (const uint8_t* const *)fr->data, (const int*) fr->linesize,
                                         ctx->pix_fmt, ctx->width, ctx->height, 1);
-                printf("%0x \n", av_adler32_update(0, (const uint8_t*)byte_buffer, number_of_written_bytes));
+                printf("%d, %10"PRId64", %10"PRId64", %8d, %8d, 0x%08"PRIx32"\n", video_stream,
+                        fr->pkt_pts, fr->pkt_dts, fr->pkt_duration,
+                        number_of_written_bytes, av_adler32_update(0, (const uint8_t*)byte_buffer, number_of_written_bytes));
             }
+            av_free_packet(&pkt);
+            av_init_packet(&pkt);
         }
+        i++;
     }
-    printf("End of file!!\n");
+    pkt.data = NULL;
+    pkt.size = 0;
+    if (pkt.pts == AV_NOPTS_VALUE)
+        pkt.pts = pkt.dts = i;
     int flag = 0;
     while (!flag) {
         get_frame = 0;
-        pkt.data = NULL;
-        pkt.size = 0;
         if (avcodec_decode_video2(ctx, fr, &get_frame, &pkt) < 0 || get_frame == 0)
             flag = 1;
-        printf("YEAAAH WE DECODED IT\n");
         if (get_frame) {
-            printf("we finally get frame\n");
             number_of_written_bytes = av_image_copy_to_buffer(byte_buffer, byte_buffer_size,
                                     (const uint8_t* const *)fr->data, (const int*) fr->linesize,
                                     ctx->pix_fmt, ctx->width, ctx->height, 1);
-            printf("%0x \n", av_adler32_update(0, (const uint8_t*)byte_buffer, number_of_written_bytes));
+            printf("%d, %10"PRId64", %10"PRId64", %8d, %8d, 0x%08"PRIx32"\n", video_stream,
+                    fr->pkt_pts, fr->pkt_dts, fr->pkt_duration,
+                    number_of_written_bytes, av_adler32_update(0, (const uint8_t*)byte_buffer, number_of_written_bytes));
         }
+        i++;
     }
+    av_free_packet(&pkt);
+    av_frame_free(&fr);
+    avcodec_close(ctx);
+    avformat_close_input(&fmt_ctx);
+    avcodec_free_context(&ctx);
+    av_freep(&byte_buffer);
     return 0;
 }
 
 int main(int argc, char **argv)
 {
-    /* register all the codecs */
     av_register_all();
-    printf("register codecs\n");
     video_decode_example(argv[1]);
-//    video_encode_example("test.h264", AV_CODEC_ID_H264);
-//    video_decode_example("test%02d.pgm", "test.mpg");
-
     return 0;
 }
 
